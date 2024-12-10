60d961ee75b4c704f06557e7997f6a8f876c4d03
Norris-Timmy <112657882+1109-Norris-Timmy@users.noreply.github.com>
2024-12-10 13:26:20 -0800

    Update houses_API.py
    Updated the call to collect the latitude and longitude coordinates for the houses as well as removed the specification in the API that searched specifically for houses that are for sale.

--- houses_API.py
+++ houses_API.py
@@ -4,7 +4,7 @@ import time
 def houses():
     url = "https://zillow56.p.rapidapi.com/search"
 
-    querystring = {"polygon":"39.723844 -120.001231,39.747606, -119.704257,39.396162 -119.700127,39.399877 -120.006381,39.723844 -120.001231","output":"json","status":"forSale","sortSelection":"priorityscore","listing_type":"by_agent","doz":"any"}
+    querystring = {"polygon":"39.723844 -120.001231,39.747606, -119.704257,39.396162 -119.700127,39.399877 -120.006381,39.723844 -120.001231","output":"json","sortSelection":"priorityscore","listing_type":"by_agent","doz":"any"}
     
     headers = {
         "x-rapidapi-key": "eb433fb3e6msh6f40dfa8c7810a1p1587fbjsn9ddafafaa1a5",
@@ -19,9 +19,11 @@ def houses():
             for i in data["results"]:
                 zpid = i.get("zpid", None) 
                 address = i.get("streetAddress", None)
-                price = i.get("price",None) 
-                if zpid and address and price:  
-                    properties.append({"zpid": zpid, "address": address, "price": price})
+                price = i.get("price",None)
+                lat = i.get("latitude",None)
+                long = i.get("longitude",None)
+                if zpid and address and price and lat and long:  
+                    properties.append({"zpid": zpid, "address": address, "price": price, "lat":lat, "long":long})
 
     #print(properties)      
     return properties
