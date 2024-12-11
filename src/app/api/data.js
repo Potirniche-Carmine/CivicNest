710184ad7776a21f13a7cb7e2657e67e00c28899
Norris-Timmy <112657882+1109-Norris-Timmy@users.noreply.github.com>
2024-12-10 16:21:54 -0800

    Update data.js

--- src/app/api/data.js
+++ src/app/api/data.js
@@ -3,7 +3,7 @@ import pool from '../../api/db';
 export default async function handler(req, res){
   try{
     const client = await pool.connect();
-    const result = await client.query('SELECT latitude, longitude FROM houses');
+    const result = await client.query('SELECT lat, long, price FROM houses');
     client.release();
     res.status(200).json(result.rows);
   }
