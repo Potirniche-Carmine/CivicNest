abf0fe16b6db7a871fcb51194ba676e97abf5cf6
KaleoSanchez <kaleo1095@gmail.com>
2025-02-07 16:15:59 -0800

    updating query to get houses

--- src/app/api/locations/route.ts
+++ src/app/api/locations/route.ts
@@ -1,20 +1,42 @@
 import { NextResponse } from 'next/server';
-import { queryDatabase } from '../../lib/db';
-import NodeCache from 'node-cache';
-const cache = new NodeCache();
+import { pool } from '@/app/lib/db';
+
+export async function POST(request: Request){
+    try{
+        const body = await request.json();
+        const { lat, long} = body;
+
+        if (!lat){
+            return NextResponse.json({ error: 'Latitude is required'}, { status: 400});
+        }
+
+        const query = `
+        SELECT lat, long FROM houses WHERE lat IS NOT NULL AND long IS NOT NULL LIMIT 3`;
+        const values = [lat, long || null];
+
+        const result = await pool.query(query, values);
+
+        const houses = result.rows[0];
+
+        return NextResponse.json({ houses}, { status: 201});
+    } catch (err) {
+        console.error('Error in Post /api/locations:', err);
+        return NextResponse.json({ error: 'Internal Server Error'}, { status: 500 });
+    }
+
+}
 
 export async function GET() {
-  const cachedData = cache.get('locations');
-  if(cachedData){
-    return NextResponse.json(cachedData, { status: 200 });
-  }
-    try {
-      // Query the database to get the house locations (latitude and longitude)
-      const result = await queryDatabase('SELECT lat, lng FROM your_table WHERE lat IS NOT NULL AND lng IS NOT NULL LIMIT 3');
-      cache.set('locations',result.rows,3600);
-      // Return the rows from the query as a JSON response
-      return NextResponse.json(result.rows, { status: 200 });
-  }catch(error){
-  return NextResponse.json({message: 'Method Not Allowed' })
-  }
+  try{const query = `
+    SELECT address, lat, long, price, zpid
+    FROM houses
+    ORDER BY address`;
+
+    const result = await pool.query(query);
+    return NextResponse.json({ houses: result.rows});
+}   catch (err) {
+    console.error('Error fetching locations:', err);
+    return NextResponse.json({error: 'Internal Server Error' }, { status: 500 });
+}
+  
 }
