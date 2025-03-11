c457061633f78da568af0a2b4220a3b773fa8000
Norris-Timmy <112657882+1109-Norris-Timmy@users.noreply.github.com>
2025-03-11 13:38:50 -0700

    Update databases.py

--- databases.py
+++ databases.py
@@ -1,17 +1,23 @@
 import psycopg2
 import houses_API
-import schools_API
+
+DB_NAME = "dcaimsa6md4201"  
+DB_USER = "ub23idbf5fs4n9" # There's a 9 at the end
+DB_PASSWORD = "pc2bb30cd6a4689cab33cc7a5833a754114d1a191680a4b9a26f8855e75f08b4a"
+DB_HOST = "c586ehe1mt2hev.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com"
+DB_PORT = "5432"
 
 # Connect to the database server
 conn = psycopg2.connect(
-    host="localhost",
-    database="civicnest_db",
-    user="brand",
-    password="Admit#762SQL"
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT
 )
 
 properties = houses_API.DataCollection()
-schools = schools_API.schools()
+
 
 
 cur = conn.cursor() # Cursor object
@@ -19,6 +25,8 @@ cur = conn.cursor() # Cursor object
 
 #Kaleo Sanchez helped with the queries for Table Creation
 # Tables:
+cur.execute('''DROP TABLE IF EXISTS houses''')
+
 cur.execute('''CREATE TABLE IF NOT EXISTS crimes (
                     crime_id SERIAL PRIMARY KEY,
                     type TEXT,
@@ -35,9 +43,9 @@ cur.execute('''CREATE TABLE IF NOT EXISTS houses (
                     long FLOAT,
                     city VARCHAR(255) NOT NULL,
                     state VARCHAR(255) NOT NULL,
-                    zipcod INT,
-                    price_change NUMERIC NOT NULL,
-                    price_per_sqft NUMERIC NOT NULL
+                    zipcode VARCHAR(255),
+                    price_change NUMERIC,
+                    price_per_sqft NUMERIC
                 )''')
 
 cur.execute('''CREATE TABLE IF NOT EXISTS schools (
@@ -50,7 +58,7 @@ cur.execute('''CREATE TABLE IF NOT EXISTS schools (
 for property in properties:
     zpid = property["zpid"]
     address = property["address"]
-    price = property["value"]
+    price = property["price"]
     lat = property["latitude"]
     long = property["longitude"]
     city = property["city"]
@@ -63,18 +71,6 @@ for property in properties:
             ON CONFLICT (zpid) DO UPDATE SET 
             address = EXCLUDED.address, price = EXCLUDED.price, lat = EXCLUDED.lat, long = EXCLUDED.long''', # This is incase we have no zpid
             (zpid, address, price,lat,long,city,state,zipcode,price_change,price_per_sqft))
-
-
-for school in schools:
-    name = school["name"]
-    grades = school["grades"]
-    rating = school["rating"]
-
-    cur.execute('''INSERT INTO schools (name, grades, rating)
-        VALUES (%s, %s, %s)
-        ON CONFLICT (name) DO NOTHING''',
-        (name, grades, rating))
-
 
 
 # Commit the changes
