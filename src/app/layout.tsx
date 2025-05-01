64b9b7dabd34fa4d18395d25ca6d9741233bae3d
Carmine <potirnichecarmine@gmail.com>
2025-05-01 09:19:58 -0700

    Fixed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,22 @@ import type { Metadata } from "next";
 import { Header } from "../components/header";
 import { Footer } from '../components/footer';
 import { ThemeProvider } from "../components/theme-provider";
-
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: "CivicNest",
   description: "A comprehensive platform providing real estate insights to help users make informed neighborhood decisions.",
+  openGraph: {
+    title: "CivicNest", 
+    description: "A comprehensive platform providing real estate insights...",
+    images: [
+      {
+        url: 'https://civicnest.carmine.live/logo.png', width: 100, height: 100, alt: 'CivicNest Logo', 
+      },
+    ],
+    type: 'website', url: 'https://civicnest.carmine.live',
+  },
 };
 
 export default function RootLayout({
